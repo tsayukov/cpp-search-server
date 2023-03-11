bcc37d15e6ae184dadeed5b5ddaf51731c9ff3eb
Pasha Tsayukov <p.tsayukov@gmail.com>
2023-03-11 19:55:52 +0300

    Merged log duration into one header

--- search-server/log_duration.h
+++ search-server/log_duration.h
@@ -22,3 +22,17 @@ private:
     std::ostream& output_stream_;
     const Clock::time_point start_time_ = Clock::now();
 };
+
+LogDuration::LogDuration(const std::string& operation_name, std::ostream& output_stream)
+        : operation_name_(operation_name)
+        , output_stream_(output_stream) {
+}
+
+LogDuration::~LogDuration() {
+    using namespace std::chrono;
+    using namespace std::literals;
+
+    const auto end_time = Clock::now();
+    const auto dur = end_time - start_time_;
+    output_stream_ << operation_name_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+}
