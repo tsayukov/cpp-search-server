16b08b9b27a17229e5e9bb404ae6d662a56e43a2
Pasha Tsayukov <p.tsayukov@gmail.com>
2023-04-10 22:12:46 +0300

    Added SplitIntoWordsView function

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -19,3 +19,22 @@ std::vector<std::string> SplitIntoWords(const std::string& text) {
 
     return words;
 }
+
+std::vector<std::string_view> SplitIntoWordsView(std::string_view text) {
+    using pos_t = std::size_t;
+    using count_t = std::size_t;
+
+    std::vector<std::string_view> words;
+
+    pos_t pos = text.find_first_not_of(' ');
+    text.remove_prefix(pos == std::string_view::npos ? text.size() : count_t(pos));
+
+    while (!text.empty()) {
+        pos_t pos_space = text.find(' ');
+        words.push_back(text.substr(pos_t(0), count_t(pos_space)));
+        pos = text.find_first_not_of(' ', pos_space);
+        text.remove_prefix(pos == std::string_view::npos ? text.size() : count_t(pos));
+    }
+
+    return words;
+}
--- search-server/string_processing.h
+++ search-server/string_processing.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 #include <vector>
 
 std::vector<std::string> SplitIntoWords(const std::string& text);
+
+std::vector<std::string_view> SplitIntoWordsView(std::string_view text);
