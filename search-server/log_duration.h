879a38cbe3f170e654d39548fd8889b0ea712712
Pasha Tsayukov <p.tsayukov@gmail.com>
2023-04-13 17:36:44 +0300

    Changed log_duration operation_name field from std::string to std::string_view

--- search-server/log_duration.h
+++ search-server/log_duration.h
@@ -4,26 +4,30 @@
 #include <iostream>
 
 #define PROFILE_CONCAT_INTERNAL(X, Y) X##Y
+
 #define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
+
 #define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
+
 #define LOG_DURATION(op_name) LogDuration UNIQUE_VAR_NAME_PROFILE(op_name)
+
 #define LOG_DURATION_STREAM(op_name, stream) LogDuration UNIQUE_VAR_NAME_PROFILE(op_name, stream)
 
 class LogDuration {
 public:
     using Clock = std::chrono::steady_clock;
 
-    explicit LogDuration(const std::string& operation_name, std::ostream& output_stream = std::cerr);
+    explicit LogDuration(std::string_view operation_name, std::ostream& output_stream = std::cerr);
 
     ~LogDuration();
 
 private:
-    const std::string operation_name_;
+    const std::string_view operation_name_;
     std::ostream& output_stream_;
     const Clock::time_point start_time_ = Clock::now();
 };
 
-LogDuration::LogDuration(const std::string& operation_name, std::ostream& output_stream)
+LogDuration::LogDuration(std::string_view operation_name, std::ostream& output_stream)
         : operation_name_(operation_name)
         , output_stream_(output_stream) {
 }
