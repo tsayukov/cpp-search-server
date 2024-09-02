7f73cdcd1f66d916e55f4ed48348879db39cc3d3
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-09-02 17:44:07 +0300

    Add benchmarks

--- benchmarks/generator.hpp
+++ benchmarks/generator.hpp
@@ -1,34 +1,91 @@
-#pragma once
+#ifndef SEARCH_SERVER_GENERATOR_HPP_
+#define SEARCH_SERVER_GENERATOR_HPP_
 
-#include <random>
+#include <search_server/search_server.hpp>
 
-class BaseGenerator {
-protected:
-    inline static std::random_device random_device_;
-    inline static auto generator_ = std::mt19937(random_device_());
-};
+#include <benchmarking/generator.hpp>
 
-template<typename T>
-class Generator : public BaseGenerator {
-public:
-    static T Get(T min = std::numeric_limits<T>::min(), T max = std::numeric_limits<T>::max()) {
-        return std::uniform_int_distribution<T>(min, max)(generator_);
+#include <algorithm>
+#include <cassert>
+#include <functional>
+#include <string>
+#include <vector>
+
+namespace search_server::generator {
+
+inline std::string GenerateWord(std::size_t max_length) {
+    assert(max_length > 0);
+
+    const auto length = benchmarking::Generator<std::size_t>::Get(1, max_length);
+    std::string word;
+    word.reserve(length);
+    for (std::size_t i = 0; i < length; ++i) {
+        word.push_back(benchmarking::Generator<char>::Get());
     }
+    return word;
+}
+
+inline std::vector<std::string> GenerateDictionary(std::size_t word_count, std::size_t max_length) {
+    assert(word_count > 0);
+
+    std::vector<std::string> words;
+    words.reserve(word_count);
+    for (std::size_t i = 0; i < word_count; ++i) {
+        words.push_back(GenerateWord(max_length));
+    }
+    std::sort(words.begin(), words.end());
+    words.erase(std::unique(words.begin(), words.end()), words.end());
+    return words;
+}
+
+inline std::string GenerateQuery(const std::vector<std::string>& dictionary,
+                                 std::size_t word_count,
+                                 double minus_prob = 0) {
+    assert(word_count > 0);
+
+    std::string query;
+    for (std::size_t i = 0; i < word_count; ++i) {
+        if (!query.empty()) {
+            query.push_back(' ');
+        }
+        if (benchmarking::Generator<double>::Get(0, 1) < minus_prob) {
+            query.push_back('-');
+        }
+        query += dictionary[benchmarking::Generator<std::size_t>::Get(0, dictionary.size() - 1)];
+    }
+    return query;
+}
+
+inline std::vector<std::string> GenerateQueries(const std::vector<std::string>& dictionary,
+                                                std::size_t query_count,
+                                                std::size_t max_word_count) {
+    std::vector<std::string> queries;
+    queries.reserve(query_count);
+    for (std::size_t i = 0; i < query_count; ++i) {
+        queries.push_back(GenerateQuery(dictionary, max_word_count));
+    }
+    return queries;
+}
+
+struct SearchServerGenerator {
+    inline static const auto dictionary = GenerateDictionary(1'000, 10);
+    inline static const auto documents = GenerateQueries(dictionary, 10'000, 70);
+    inline static const auto query = GenerateQuery(dictionary, 500, 0.1);
 };
 
-template<>
-class Generator<char> : public BaseGenerator {
-public:
-    static char Get(char min = 'a', char max = 'z') {
-        return static_cast<char>(std::uniform_int_distribution<int>(min, max)(generator_));
+inline const SearchServer const_search_server = std::invoke([] {
+    SearchServer search_server(SearchServerGenerator::dictionary[0]);
+    const std::vector<int> ratings = {1, 2, 3};
+    for (std::size_t i = 0; i < SearchServerGenerator::documents.size(); ++i) {
+        int document_id = static_cast<int>(i);
+        search_server.AddDocument(document_id,
+                                  SearchServerGenerator::documents[i],
+                                  DocumentStatus::kActual,
+                                  ratings);
     }
-};
+    return search_server;
+});
 
-template<>
-class Generator<double> : public BaseGenerator {
-public:
-    static double Get(double min = std::numeric_limits<double>::min(),
-                      double max = std::numeric_limits<double>::max()) {
-        return std::uniform_real_distribution<double>(min, max)(generator_);
-    }
-};
+} // namespace search_server::generator
+
+#endif // SEARCH_SERVER_GENERATOR_HPP_
--- benchmarks/search_server_benchmarks.cpp
+++ benchmarks/search_server_benchmarks.cpp
@@ -1,93 +1,17 @@
 #include "generator.hpp"
-#include "log_duration.hpp"
-#include "search_server/search_server.hpp"
+
+#include <benchmarking/benchmarking.hpp>
 
 #include <execution>
 #include <iostream>
-#include <string>
 #include <string_view>
-#include <vector>
-#include <cassert>
 
-namespace benchmark_tests {
+using namespace search_server;
 
-using namespace std::string_literals;
-using namespace unit_test_tools;
-
-inline std::string GenerateWord(std::size_t max_length) {
-    assert(max_length > 0);
-
-    const auto length = Generator<std::size_t>::Get(1, max_length);
-    std::string word;
-    word.reserve(length);
-    for (std::size_t i = 0; i < length; ++i) {
-        word.push_back(Generator<char>::Get());
-    }
-    return word;
-}
-
-inline std::vector<std::string> GenerateDictionary(std::size_t word_count, std::size_t max_length) {
-    assert(word_count > 0);
-
-    std::vector<std::string> words;
-    words.reserve(word_count);
-    for (std::size_t i = 0; i < word_count; ++i) {
-        words.push_back(GenerateWord(max_length));
-    }
-    sort(words.begin(), words.end());
-    words.erase(unique(words.begin(), words.end()), words.end());
-    return words;
-}
-
-inline std::string GenerateQuery(const std::vector<std::string>& dictionary,
-                                 std::size_t word_count, double minus_prob = 0) {
-    assert(word_count > 0);
-
-    std::string query;
-    for (std::size_t i = 0; i < word_count; ++i) {
-        if (!query.empty()) {
-            query.push_back(' ');
-        }
-        if (Generator<double>::Get(0, 1) < minus_prob) {
-            query.push_back('-');
-        }
-        query += dictionary[Generator<std::size_t>::Get(0, dictionary.size() - 1)];
-    }
-    return query;
-}
-
-inline std::vector<std::string> GenerateQueries(const std::vector<std::string>& dictionary,
-                                                std::size_t query_count, std::size_t max_word_count) {
-    std::vector<std::string> queries;
-    queries.reserve(query_count);
-    for (std::size_t i = 0; i < query_count; ++i) {
-        queries.push_back(GenerateQuery(dictionary, max_word_count));
-    }
-    return queries;
-}
-
-struct SearchServerGenerator {
-    inline static const auto dictionary = GenerateDictionary(1'000, 10);
-    inline static const auto documents = GenerateQueries(dictionary, 10'000, 70);
-    inline static const auto query = GenerateQuery(dictionary, 500, 0.1);
-};
-
-inline const SearchServer const_search_server = [] {
-    SearchServer search_server(SearchServerGenerator::dictionary[0]);
-    const std::vector<int> ratings = {1, 2, 3};
-    for (std::size_t i = 0; i < SearchServerGenerator::documents.size(); ++i) {
-        int document_id = static_cast<int>(i);
-        search_server.AddDocument(document_id, SearchServerGenerator::documents[i], DocumentStatus::kActual, ratings);
-    }
-    return search_server;
-}();
-
-// BENCHMARK TESTS
-
-template<typename ExecutionPolicy>
-void TestRemoveDocument(std::string_view mark, const ExecutionPolicy& policy) {
-    SearchServer search_server = const_search_server;
-    std::cerr << "Benchmarking of "s << mark <<" RemoveDocument:\n"s;
+template <typename ExecutionPolicy>
+void RemoveDocumentImpl(std::string_view mark, const ExecutionPolicy& policy) {
+    SearchServer search_server = generator::const_search_server;
+    std::cerr << "Benchmarking of " << mark <<" RemoveDocument:\n";
     {
         LOG_DURATION(mark);
         const int document_count = search_server.GetDocumentCount();
@@ -98,27 +22,41 @@ void TestRemoveDocument(std::string_view mark, const ExecutionPolicy& policy) {
     }
 }
 
-template<typename ExecutionPolicy>
-void TestMatchDocument(std::string_view mark, const ExecutionPolicy& policy) {
-    const SearchServer& search_server = const_search_server;
-    std::cerr << "Benchmarking of "s << mark <<" MatchDocument:\n"s;
+RUN(RemoveDocument) {
+    RemoveDocumentImpl("seq", std::execution::seq);
+    RemoveDocumentImpl("par", std::execution::par);
+}
+
+template <typename ExecutionPolicy>
+void MatchDocumentImpl(std::string_view mark, const ExecutionPolicy& policy) {
+    const SearchServer& search_server = generator::const_search_server;
+    std::cerr << "Benchmarking of " << mark <<" MatchDocument:\n";
     {
         LOG_DURATION(mark);
         const int document_count = search_server.GetDocumentCount();
         int word_count = 0;
         for (int id = 0; id < document_count; ++id) {
-            const auto& [words, status] = search_server.MatchDocument(policy, SearchServerGenerator::query, id);
+            const auto& [words, status] = search_server.MatchDocument(
+                    policy,
+                    generator::SearchServerGenerator::query,
+                    id);
             word_count += words.size();
         }
         std::cout << word_count << std::endl;
     }
 }
 
-template<typename ExecutionPolicy>
-void TestFindTopDocuments(std::string_view mark, const ExecutionPolicy& policy) {
-    const SearchServer& search_server = const_search_server;
-    std::cerr << "Benchmarking of "s << mark <<" FindTopDocuments:\n"s;
-    const auto queries = GenerateQueries(SearchServerGenerator::dictionary, 100, 70);
+RUN(MatchDocument) {
+    MatchDocumentImpl("seq", std::execution::seq);
+    MatchDocumentImpl("par", std::execution::par);
+}
+
+template <typename ExecutionPolicy>
+void FindTopDocumentsImpl(std::string_view mark, const ExecutionPolicy& policy) {
+    const SearchServer& search_server = generator::const_search_server;
+    std::cerr << "Benchmarking of " << mark <<" FindTopDocuments:\n";
+    const auto queries = generator::GenerateQueries(
+            generator::SearchServerGenerator::dictionary, 100, 70);
     {
         LOG_DURATION(mark);
         double total_relevance = 0.0;
@@ -131,17 +69,7 @@ void TestFindTopDocuments(std::string_view mark, const ExecutionPolicy& policy) 
     }
 }
 
-} // namespace benchmark_tests
-
-void RunAllBenchmarkTests() {
-    using namespace benchmark_tests;
-
-    TestRemoveDocument("seq", std::execution::seq);
-    TestRemoveDocument("par", std::execution::par);
-
-    TestMatchDocument("seq", std::execution::seq);
-    TestMatchDocument("par", std::execution::par);
-
-    TestFindTopDocuments("seq", std::execution::seq);
-    TestFindTopDocuments("par", std::execution::par);
+RUN(FindTopDocuments) {
+    FindTopDocumentsImpl("seq", std::execution::seq);
+    FindTopDocumentsImpl("par", std::execution::par);
 }
