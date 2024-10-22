56843232e384ffe02d8710ef9255f2b80020e0f7
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-10-22 22:09:12 +0300

    Add documentation

--- include/search_server/process_queries.hpp
+++ include/search_server/process_queries.hpp
@@ -1,3 +1,6 @@
+/// \file process_queries.hpp
+/// \brief Functions for processing queries to \c search_server::SearchServer
+
 #ifndef SEARCH_SERVER_PROCESS_QUERIES_HPP_
 #define SEARCH_SERVER_PROCESS_QUERIES_HPP_
 
@@ -9,10 +12,14 @@
 
 namespace search_server {
 
+/// Process \c queries for given \c searchServer and put \c std::vector<Document>
+/// for each query into \c std::vector.
 SEARCH_SERVER_EXPORT
 std::vector<std::vector<Document>> processQueries(const SearchServer& searchServer,
                                                   const std::vector<std::string>& queries);
 
+/// Join the results of each query of the corresponding \c search_server::processQueries() call
+/// into one \c std::vector.
 SEARCH_SERVER_EXPORT
 std::vector<Document> processQueriesJoined(const SearchServer& searchServer,
                                            const std::vector<std::string>& queries);
