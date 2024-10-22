469390ff79064e6dbc74f321a5901d8e41bb0499
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-10-22 15:00:06 +0300

    Use asserts; turn off benchmarking of slow parallel methods

--- benchmarks/search_server_benchmarks.cpp
+++ benchmarks/search_server_benchmarks.cpp
@@ -1,73 +1,122 @@
 #include "generator.hpp"
 
+#include "search_server/search_server.hpp"
+
+#include <chrono>
 #include <execution>
 #include <iostream>
 #include <string_view>
 
 #include <benchmarking/benchmarking.hpp>
+#include <testing/assertions.hpp>
 
 using namespace search_server;
 
+using ms = std::chrono::milliseconds;
+using namespace std::chrono_literals;
+
 template <typename ExecutionPolicy>
-void removeDocumentImpl(std::string_view mark, const ExecutionPolicy& policy) {
-    SearchServer searchServer = generator::kConstSearchServer;
-    std::cerr << "Benchmarking of " << mark << " removeDocument:\n";
+auto matchDocumentImpl(const ExecutionPolicy& policy,
+                       const search_server::SearchServer& searchServer,
+                       const std::vector<std::string>& queries) {
+    const int documentCount = searchServer.getDocumentCount();
+    int wordCount = 0;
+    ms duration = 0ms;
+    ms durationPerQuery = 0ms;
+    for (std::size_t i = 0; i < queries.size(); ++i, duration += durationPerQuery) {
+        LOG_DURATION(&durationPerQuery);
+        for (int id = 0; id < documentCount; ++id) {
+            const auto& [words, _] = searchServer.matchDocument(policy, queries[i], id);
+            wordCount += words.size();
+        }
+    }
+
+    // To prevent optimization of the benchmarking code
+    std::cout << wordCount << std::endl;
+
+    return static_cast<double>(duration.count());
+}
+
+RUN(MatchDocument) {
+    const auto& searchServer = generator::kConstSearchServer;
+    const auto queries = generator::generateQueries(generator::kDictionary, 10, 500);
+
+    auto par = matchDocumentImpl(std::execution::par, searchServer, queries);
+    std::cout << "MatchDocument (par): " << par << '\n';
+    auto seq = matchDocumentImpl(std::execution::seq, searchServer, queries);
+    std::cout << "MatchDocument (seq): " << seq << '\n';
+    auto quot = seq / par;
+    std::cout << "Quot: " << quot << std::endl;
+    ASSERT(quot - 1.5 >= 1e-6);
+}
+
+#if 0
+
+template <typename ExecutionPolicy>
+auto removeDocumentImpl(const ExecutionPolicy& policy) {
+    auto searchServer = generator::generateSearchServer(100'000, 70);
+    const int documentCount = searchServer.getDocumentCount();
+    ms duration;
     {
-        LOG_DURATION(mark);
-        const int documentCount = searchServer.getDocumentCount();
+        LOG_DURATION(&duration);
         for (int id = 0; id < documentCount; ++id) {
             searchServer.removeDocument(policy, id);
         }
-        std::cout << searchServer.getDocumentCount() << std::endl;
     }
+
+    // To prevent optimization of the benchmarking code
+    std::cout << searchServer.getDocumentCount() << std::endl;
+
+    return static_cast<double>(duration.count());
 }
 
-RUN(removeDocument) {
-    removeDocumentImpl("seq", std::execution::seq);
-    removeDocumentImpl("par", std::execution::par);
+RUN(RemoveDocument) {
+    auto par = removeDocumentImpl(std::execution::par);
+    std::cout << "RemoveDocument (par): " << par << '\n';
+    auto seq = removeDocumentImpl(std::execution::seq);
+    std::cout << "RemoveDocument (seq): " << seq << '\n';
+    auto quot = seq / par;
+    std::cout << "Quot: " << quot << std::endl;
+    ASSERT(quot - 1.0 >= 1e-6);
 }
 
 template <typename ExecutionPolicy>
-void matchDocumentImpl(std::string_view mark, const ExecutionPolicy& policy) {
-    const SearchServer& searchServer = generator::kConstSearchServer;
-    std::cerr << "Benchmarking of " << mark << " matchDocument:\n";
+auto findTopDocumentsImpl(const ExecutionPolicy& policy,
+                          const search_server::SearchServer& searchServer,
+                          const std::vector<std::string>& queries) {
+    ms duration;
+    std::vector<std::vector<search_server::Document>> result;
+    result.reserve(queries.size());
     {
-        LOG_DURATION(mark);
-        const int documentCount = searchServer.getDocumentCount();
-        int wordCount = 0;
-        for (int id = 0; id < documentCount; ++id) {
-            const auto& [words, status] =
-                    searchServer.matchDocument(policy, generator::SearchServerGenerator::query, id);
-            wordCount += words.size();
-        }
-        std::cout << wordCount << std::endl;
-    }
-}
-
-RUN(matchDocument) {
-    matchDocumentImpl("seq", std::execution::seq);
-    matchDocumentImpl("par", std::execution::par);
-}
-
-template <typename ExecutionPolicy>
-void findTopDocumentsImpl(std::string_view mark, const ExecutionPolicy& policy) {
-    const SearchServer& searchServer = generator::kConstSearchServer;
-    std::cerr << "Benchmarking of " << mark << " findTopDocuments:\n";
-    const auto queries =
-            generator::GenerateQueries(generator::SearchServerGenerator::dictionary, 100, 70);
-    {
-        LOG_DURATION(mark);
-        double totalRelevance = 0.0;
+        LOG_DURATION(&duration);
         for (const auto& query : queries) {
-            for (const auto& document : searchServer.findTopDocuments(policy, query)) {
-                totalRelevance += document.relevance;
-            }
+            result.emplace_back(searchServer.findTopDocuments(policy, query));
         }
-        std::cout << totalRelevance << std::endl;
     }
+
+    // To prevent optimization of the benchmarking code
+    double totalRelevance = 0.0;
+    for (const auto& documents : result) {
+        for (const auto& doc : documents) {
+            totalRelevance += doc.relevance;
+        }
+    }
+    std::cout << totalRelevance << std::endl;
+
+    return static_cast<double>(duration.count());
 }
 
-RUN(findTopDocuments) {
-    findTopDocumentsImpl("seq", std::execution::seq);
-    findTopDocumentsImpl("par", std::execution::par);
+RUN(FindTopDocuments) {
+    const auto& searchServer = generator::kConstSearchServer;
+    const auto queries = generator::generateQueries(generator::kDictionary, 1'000, 500);
+
+    auto par = findTopDocumentsImpl(std::execution::par, searchServer, queries);
+    std::cout << "FindTopDocuments (par): " << par << '\n';
+    auto seq = findTopDocumentsImpl(std::execution::seq, searchServer, queries);
+    std::cout << "FindTopDocuments (seq): " << seq << '\n';
+    auto quot = seq / par;
+    std::cout << "Quot: " << quot << std::endl;
+    ASSERT(quot - 1.0 >= 1e-6);
 }
+
+#endif
