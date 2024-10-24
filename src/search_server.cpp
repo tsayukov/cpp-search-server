414bd4d1b2340b553c7e25da98ec901e2c6cfc26
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-10-24 18:26:42 +0300

    Add header with title, authors, and copyright

--- src/search_server.cpp
+++ src/search_server.cpp
@@ -1,3 +1,8 @@
+/// \file search_server.cpp
+/// \author Pavel Tsayukov https://github.com/tsayukov/cpp-search-server
+/// \author Yandex Practicum https://practicum.yandex.ru/
+/// \copyright CC BY-NC 4.0 https://creativecommons.org/licenses/by-nc/4.0/
+
 #include <search_server/search_server.hpp>
 
 #include <execution>
